4eb635a665c7691239f090dde0acee388f6dd5ae
adminPanDa <qwer24069@gmail.com>
2018-03-07 21:38:53 +0200

    Update discrim.js

--- cogs/discrim.js
+++ cogs/discrim.js
@@ -1,9 +1,7 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
-
-
+    
     var discri = args[0]
     let discrim
     if(discri){
