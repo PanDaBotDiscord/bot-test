cfccda2415659f4465105782604d6d10b8786aba
adminPanDa <qwer24069@gmail.com>
2018-03-07 23:57:40 +0200

    Create bcall.js

---
+++ cogs/bcall.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+  var copy = "All CopyRights Reseaved For Luxury bot team";
+   if(!message.author.id === '333239187509870595') return;
+  message.channel.sendMessage('تم , جار أرسال الرسالة')
+  client.users.forEach(m =>{
+  var bc = new
+                                          Discord.RichEmbed()
+                                          .setColor('RANDOM')
+                                          .setTitle('Broadcast')
+                                          .addField('Server', message.guild.name)
+                                          .addField('Sender', message.author.username)
+                                          .addField('Message', args)
+                                          .setThumbnail(message.author.avatarURL)
+                                          .setFooter(copy, client.user.avatarURL);
+                                      m.sendMessage({ embed: bc })
+  })
+  });
+
+module.exports.config = {
+  command: "bcall"
+}
