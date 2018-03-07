96456880c92ca34a4c72b201398bbf2edd91ada3
adminPanDa <qwer24069@gmail.com>
2018-03-07 21:04:16 +0200

    Update ping.js

--- cogs/ping.js
+++ cogs/ping.js
@@ -1,4 +1,4 @@
-onst Discord = require("discord.js");
+const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
