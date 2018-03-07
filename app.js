ecd6aef20d545ffbeb38b5919af8ddef9cccc37b
adminPanDa <qwer24069@gmail.com>
2018-03-07 20:21:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 const fs = require("fs");
-const profanities = require('profanities');
 //------------end require ----------//
 
 //-----------strat var global -------------//
