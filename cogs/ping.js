ad06738e351ea075750fc7adae41e91cc72c2505
adminPanDa <qwer24069@gmail.com>
2018-03-08 02:47:15 +0200

    Update ping.js

--- cogs/ping.js
+++ cogs/ping.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-        message.channel.send('Pong!')
+       const m = await message.channel.send("Ping!");
+	 m.edit(`:ping_pong: Pong! (\`${Date.now() - message.createdTimestamp}\`ms) - API: (\`${Math.round(client.ping)}\`ms)`);
+
     }
 
 module.exports.config = {
