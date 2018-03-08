3a94dc09ec15ed21641a7d3971da70fccbf9e6ef
adminPanDa <qwer24069@gmail.com>
2018-03-08 19:58:29 +0200

    Update bcall.js

--- cogs/bcall.js
+++ cogs/bcall.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
-module.exports.run = async (bot, message, args) => {
-  var argm = message.content.split(' ').slice(1);
+var prefix = '&';
+module.exports.run = async (bot, message,) => {
+let args = message.content.split(' ').slice(1).join(' ');
   var copy = "All CopyRights Reseaved For Luxury bot team";
    if(!message.author.id === '333239187509870595') return;
   message.channel.sendMessage('تم , جار أرسال الرسالة')
@@ -10,7 +11,7 @@ module.exports.run = async (bot, message, args) => {
                                           .setTitle('Broadcast')
                                           .addField('Server', message.guild.name)
                                           .addField('Sender', message.author.username)
-                                          .addField('Message', argm)
+                                          .addField('Message', args)
                                           .setThumbnail(message.author.avatarURL)
                                           .setFooter(copy, bot.user.avatarURL);
                                       m.sendMessage({ embed: bc })
