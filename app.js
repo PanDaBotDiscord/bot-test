6c914866428f9d0877395baeffa2593ea199df09
adminPanDa <qwer24069@gmail.com>
2018-03-11 01:19:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -132,43 +132,6 @@ bot.on('message', message => {
     //if conditnon
 });
 
-bot.on('message' , message => {
-    if (message.content === "!uptime") {
-if(!message.channel.guild) return;
-const embed = new Discord.RichEmbed()
- .setTitle("Security Bot Uptime:")
- .setDescription(`${secondsToString(process.uptime())}`)
- .setColor("#08088A")
-.setFooter(bot.user.tag, bot.user.displayAvatarURL);       
-message.channel.send(embed)
-    }
-});
-
-function convertMS(ms) {
-    var d, h, m, s;
-    s = Math.floor(ms / 1000);
-    m = Math.floor(s / 60);
-    s = s % 60;
-    h = Math.floor(m / 60);
-    m = m % 60;
-    d = Math.floor(h / 24);
-    h = h % 24;
-    return {
-        d: d,
-        h: h,
-        m: m,
-        s: s
-    };
-};
-
-function secondsToString(seconds) {
-	seconds = Math.trunc(seconds)
-	let numdays = Math.floor((seconds % 31536000) / 86400)
-	let numhours = Math.floor(((seconds % 31536000) % 86400) / 3600)
-	let numminutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60)
-	let numseconds = (((seconds % 31536000) % 86400) % 3600) % 60
-	return `${numdays} days ${numhours} hours ${numminutes} minutes ${numseconds} seconds`
-}
 
     
 //consol log started bot
